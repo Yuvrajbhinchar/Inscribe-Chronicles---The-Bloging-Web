44bde9f8973debd418071a5cb41236d30036e29a
Yuvraj <yuvrajbhinchar00@gmail.com>
2025-01-10 20:15:42 +0300

    first commit

---
+++ ForntEnd/Inscribe Chronicles/src/componant/BlogCard.jsx
@@ -0,0 +1,31 @@
+function BlogCard({ blog }) {
+  return (
+    // Main Div
+    <div>
+        {/* 1 */}
+        {/* Basic Intro */}
+        <div>
+            {/* Logo */}
+            <div></div>
+            <h1>In ? by ?</h1>
+        </div>
+
+        {/* 2 */}
+        {/* Blog Content */}
+        <div>
+        <div>
+            <h1>Blog Title</h1>
+            <p>Blog Content</p>
+        </div>
+         {/* Blog Image */}
+         <div></div>
+        </div>
+
+        {/* 3 */}
+        <div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>
+  );
+}
